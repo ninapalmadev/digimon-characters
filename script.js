b19e8c7e90676ad09a5afa364261cc3bafcd4408
ninapalmadev <ninapalmadev@gmail.com>
2022-06-09 18:14:16 -0300

    refatora styles

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const digimonMain = document.querySelector('.section-characteres')
 const searchLevel = document.querySelector('#searchBar')
+let favoritosHeader = document.querySelector('#favoritosNav')
+
+let digimonFavoritos = [];
 
 async function loadDigimons (){
     const response = await fetch('https://digimon-api.vercel.app/api/digimon');
@@ -13,7 +16,6 @@ loadDigimons ();
 function createDigimonCards(data){
     digimonMain.innerHTML = '';
     data.map(digimon => {
-       // console.log(data);
         const {name, img, level} = digimon;
         const digimonsCards = document.createElement('figure');
         digimonsCards.classList.add('.description');
@@ -22,11 +24,53 @@ function createDigimonCards(data){
                 <img class="img-description" src="${img}" width="180" alt="Imagem do Digimon ${name}">
                     <figcaption class="name-character">${name}</figcaption>
                     <span class="span-dgm ${level}">Level: ${level}</span>
+                    <button type="button" class="favorite-btn" onclick='favDigimon(${JSON.stringify(digimon)})'>
+                    +
+                    </button>    
             </div>
         `            
-    digimonMain.appendChild(digimonsCards);
+        digimonMain.appendChild(digimonsCards);
+    
     });  
 };
+            
+function favDigimon(digimon) {
+    digimonFavoritos = digimonLocalStorage();
+    const addFavoritos = digimonFavoritos.filter((item) => (item.name === digimon.name))
+    if(addFavoritos.length === 0) {
+        digimonFavoritos.push(digimon)
+    }else{
+        digimonFavoritos = digimonFavoritos.filter((item) => (item.name != digimon.name))
+    }
+    localStorage.setItem("Lista Digimon", JSON.stringify(digimonFavoritos))
+};
+
+function digimonLocalStorage() {
+    if(localStorage.getItem("Lista Digimon") == null){
+        localStorage.setItem("Lista Digimon", "[]")
+    }
+    const dataFavoritos = localStorage.getItem("Lista Digimon")
+    return JSON.parse(dataFavoritos)
+}
+
+function carregarFavDigimons(digimonMain){
+    if(localStorage.getItem("Lista Digimon")){
+    const favoritesTable = document.querySelector('#favoritos tbody');
+    const digimonsFavoritados = digimonLocalStorage();
+
+        digimonMain.forEach(digimon => {
+            const tr = document.createElement('tr')
+            tr.innerHTML = `
+                <td>
+                    <img src"${digimon.img} alt ="Imagem do Digimon ${digimon.name}"
+                </td>
+                <td>${digimon.name}</td>
+                <td>${digimon.level}</td>
+            `;
+            favoritesTable.appendChild(tr);
+        })
+    }
+}
 
 const hideDigimons = (searchDigimons, inputValue) => {
     searchDigimons
@@ -47,11 +91,10 @@ const showDigimons = (searchDigimons, inputValue) => {
 searchLevel.addEventListener('input', event => {
     const inputValue = event.target.value.toLowerCase();
     const searchDigimons = Array.from(digimonMain.children);
-
+ 
     hideDigimons(searchDigimons, inputValue);
     showDigimons(searchDigimons, inputValue);
 });
-
 
 function filterDigimons(value) {
     let buttons = document.querySelectorAll(".button-value");
@@ -72,8 +115,7 @@ function filterDigimons(value) {
                 element.parentElement.classList.remove("hide");
             }else{
                 element.parentElement.classList.add("hide");
-            }
+            };
         };
     }); 
 };
-
